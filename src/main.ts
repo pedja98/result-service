ba4d7eb6cf16967aad13c802537ffc926423743a
pedja98 <predragmirkovic1998@gmail.com>
2026-06-20 23:18:11 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api/v1')
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  const config = new DocumentBuilder()
+    .setTitle('My API Title')
+    .setDescription('The API description details')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
--- src/modules/attempts/dto/submit-attempt.dto.ts
+++ src/modules/attempts/dto/submit-attempt.dto.ts
@@ -1,11 +1,24 @@
-import { IsInt, IsNotEmpty } from 'class-validator'
+import { IsInt, IsNotEmpty, IsObject } from 'class-validator'
 import { IsAnswersMap } from '../validators/is-answer-map.validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class SubmitAttemptDto {
   @IsNotEmpty()
   @IsInt()
   attemptId!: number
 
+  @ApiProperty({
+    type: 'object',
+    description: 'A key-value map where the key is the answer id and the value is the chosen correct option for that multiple-choice question.',
+    additionalProperties: {
+      type: 'integer',
+    },
+    example: {
+      1: 2,
+      2: 0,
+    },
+  })
   @IsAnswersMap()
+  @IsObject()
   answers!: Record<number, number>
 }
